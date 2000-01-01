5e1a593076e6a44122f4bf714c30bde5a5608331
root <root@beaglebone.(none)>
2000-01-01 02:20:28 +0000

    added status feature

--- LEDcpp/makeLED.cpp
+++ LEDcpp/makeLED.cpp
@@ -4,7 +4,8 @@
     This program uses USR LED 0 and can be executed in three ways:
          makeLED on
          makeLED off
-         makeLED flash
+         makeLED flash  (flash at 100ms intervals - on 50ms/off 50ms)
+         makeLED status (get the trigger status)
 */
 
 #include<iostream>
@@ -14,29 +15,34 @@ using namespace std;
 
 #define LED0_PATH "/sys/class/leds/beaglebone:green:usr0"
 
+void removeTrigger(){
+   // remove the trigger from the LED
+   std::fstream fs;
+   fs.open( LED0_PATH "/trigger", std::fstream::out);
+   fs << "none";
+   fs.close();
+}
+
 int main(int argc, char* argv[]){
    if(argc!=2){
       cout << "Usage is makeLED and one of on, off or flash" << endl;
       cout << "e.g. makeLED flash" << endl;
    }
-   string cmd(argv[1]);
 
+   string cmd(argv[1]);
    std::fstream fs;
    cout << "Starting the LED flash program" << endl;
-   cout << "The LED Path is: " << LED0_PATH << endl;
-
-   // remove the trigger from the LED
-   fs.open( LED0_PATH "/trigger", std::fstream::out);
-   fs << "none";
-   fs.close();   
+   cout << "The LED Path is: " << LED0_PATH << endl;     
 
    // select whether it is on, off or flash
    if(cmd=="on"){
+       removeTrigger();
        fs.open (LED0_PATH "/brightness", std::fstream::out);
        fs << "1";
        fs.close();
    }
    else if (cmd=="off"){
+       removeTrigger();
        fs.open (LED0_PATH "/brightness", std::fstream::out);
        fs << "0";
        fs.close();
@@ -51,6 +57,13 @@ int main(int argc, char* argv[]){
        fs.open (LED0_PATH "/delay_off", std::fstream::out);
        fs << "50";
        fs.close();
+   }
+   else if (cmd=="status"){
+   	// display the current trigger details
+   	fs.open( LED0_PATH "/trigger", std::fstream::in);
+   	string line;
+   	while(getline(fs,line)) cout << line;
+   	fs.close();      
    }
    else{
        cout << "Invalid command" << endl;
