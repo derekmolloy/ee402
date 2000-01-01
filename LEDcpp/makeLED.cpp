319ce2bd7ac524a4340e8aa241fd7f64c9cce7a4
root <root@beaglebone.(none)>
2000-01-01 02:37:08 +0000

    fixed formatting

--- LEDcpp/makeLED.cpp
+++ LEDcpp/makeLED.cpp
@@ -25,8 +25,9 @@ void removeTrigger(){
 
 int main(int argc, char* argv[]){
    if(argc!=2){
-      cout << "Usage is makeLED and one of on, off or flash" << endl;
-      cout << "e.g. makeLED flash" << endl;
+	cout << "Usage is makeLED and one of: on, off, flash or status" 
+<< endl;
+	cout << "e.g. makeLED flash" << endl;
    }
 
    string cmd(argv[1]);
@@ -36,37 +37,37 @@ int main(int argc, char* argv[]){
 
    // select whether it is on, off or flash
    if(cmd=="on"){
-       removeTrigger();
-       fs.open (LED0_PATH "/brightness", std::fstream::out);
-       fs << "1";
-       fs.close();
+	removeTrigger();
+	fs.open (LED0_PATH "/brightness", std::fstream::out);
+	fs << "1";
+	fs.close();
    }
    else if (cmd=="off"){
-       removeTrigger();
-       fs.open (LED0_PATH "/brightness", std::fstream::out);
-       fs << "0";
-       fs.close();
+	removeTrigger();
+	fs.open (LED0_PATH "/brightness", std::fstream::out);
+	fs << "0";
+	fs.close();
    }
    else if (cmd=="flash"){
-       fs.open (LED0_PATH "/trigger", std::fstream::out);
-       fs << "timer";
-       fs.close();
-       fs.open (LED0_PATH "/delay_on", std::fstream::out);
-       fs << "50";
-       fs.close();
-       fs.open (LED0_PATH "/delay_off", std::fstream::out);
-       fs << "50";
-       fs.close();
+	fs.open (LED0_PATH "/trigger", std::fstream::out);
+	fs << "timer";
+	fs.close();
+	fs.open (LED0_PATH "/delay_on", std::fstream::out);
+	fs << "50";
+	fs.close();
+	fs.open (LED0_PATH "/delay_off", std::fstream::out);
+	fs << "50";
+	fs.close();
    }
    else if (cmd=="status"){
-   	// display the current trigger details
-   	fs.open( LED0_PATH "/trigger", std::fstream::in);
-   	string line;
-   	while(getline(fs,line)) cout << line;
-   	fs.close();      
+	// display the current trigger details
+	fs.open( LED0_PATH "/trigger", std::fstream::in);
+	string line;
+	while(getline(fs,line)) cout << line;
+	fs.close();      
    }
    else{
-       cout << "Invalid command" << endl;
+	cout << "Invalid command" << endl;
    }
    cout << "Finished the LED flash program" << endl;
    return 0;
